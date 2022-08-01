8f475df3aea3c501af64506ffed71a174b7d9880
Evelyn Cristini Oliveira <evelyn.oliveira@mercadolivre.com>
2022-07-31 21:30:20 -0300

    Inicializa projeto Java

---
+++ src/main/java/com/example/dhbootcamp/meli/obterdiploma/ObterDiplomaApplication.java
@@ -0,0 +1,13 @@
+package com.example.dhbootcamp.meli.obterdiploma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObterDiplomaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObterDiplomaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dhbootcamp/meli/obterdiploma/ObterDiplomaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dhbootcamp.meli.obterdiploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObterDiplomaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
